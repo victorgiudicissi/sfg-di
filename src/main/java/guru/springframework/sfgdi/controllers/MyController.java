3fa7a62ae0afc48d2cb0b8da584647694808fec5
Victor Giudicissi <victor.giudicissi@exactaworks.com.br>
2020-06-02 15:30:55 -0300

    Usando IoC do Spring

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 public class MyController {
 
     public String sayHello() {
-        System.out.println("Hello world!");
-
         return "Hi Folks!";
     }
 }
